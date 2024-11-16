449ec25511cbd3b3b55500ce767ae6de06e6d559
Weslley Addson <wesquel@hotmail.com>
2024-11-16 18:24:59 -0300

    init commit

--- src/main/java/br/com/addson/greetingservice/controllers/GreetingController.java
+++ src/main/java/br/com/addson/greetingservice/controllers/GreetingController.java
@@ -1,2 +1,33 @@
-package br.com.addson.greetingservice.controllers;public class GreetingController {
+package br.com.addson.greetingservice.controllers;
+
+import br.com.addson.greetingservice.config.GreetingConfiguration;
+import br.com.addson.greetingservice.models.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "%s, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingConfiguration greetingConfiguration;
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value="name",
+                    defaultValue = "") String name) {
+
+        if(name.isEmpty()) name = greetingConfiguration.getDefaultValue();
+
+        return new Greeting(
+                counter.incrementAndGet(),
+                String.format(template, greetingConfiguration.getGreeting(), name)
+        );
+    }
 }
--- src/main/java/br/com/addson/greetingservice/models/Greeting.java
+++ src/main/java/br/com/addson/greetingservice/models/Greeting.java
@@ -1,2 +1,21 @@
-package br.com.addson.greetingservice.models;public class Greeting {
+package br.com.addson.greetingservice.models;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
 }
